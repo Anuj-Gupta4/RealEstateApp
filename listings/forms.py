be596bca5c41c7639c5d637eaff36884e6b47f5d
Anuj-Gupta4 <anujgupta.4388@gmail.com>
2022-12-09 19:28:55 +0545

    form and search page changes

--- listings/forms.py
+++ listings/forms.py
@@ -21,8 +21,20 @@ class ListingForm(forms.ModelForm):
         "Contact_number", 
         "Contact_mail",
         "Image",
-        # "is_sold"
+        "is_sold",
+        # "Description"
         ]
+        labels = {
+            "is_sold": "Sell Status (Please tick if your real estate has already been sold.)",
+            "Price": "Price (in NPR)",
+            "Area": "Area (in square feet)",
+            "Build_Area": "Build Area (in square feet)",
+            "Road_Width": "Road Width (in feet)",
+            "Bedroom": "Number of Bedrooms",
+            "Bathroom": "Number of Bathrooms",
+            "Floors": "Number of Floors",
+            "Parking": "Number of Parking",
+        }
         widgets = {
             'Title':forms.TextInput(attrs={'class':'form-control'}),
             'Location':forms.TextInput(attrs={'class':'form-control'}),
@@ -38,9 +50,10 @@ class ListingForm(forms.ModelForm):
             'Road_Type':forms.TextInput(attrs={'class':'form-control'}),
             'Build_Area':forms.NumberInput(attrs={'class':'form-control'}),
             'Amenities':forms.TextInput(attrs={'class':'form-control'}),
+            # 'Description':forms.Textarea(attrs={'class':'form-control'}),
             'Contact_number':forms.NumberInput(attrs={'class':'form-control'}),
-            'Contact_mail':forms.TextInput(attrs={'class':'form-control'}),
+            'Contact_mail':forms.EmailInput(attrs={'class':'form-control'}),
             'Image':forms.ClearableFileInput(attrs={'class':'form-control'}),
-            # 'is_sold':forms.CheckboxInput(attrs={'class':'form-control'}),
+            'is_sold':forms.CheckboxInput(attrs={'class':'w-8 h-8'}),
         }
         
