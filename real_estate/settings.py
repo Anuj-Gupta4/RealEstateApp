bd4af7e49c3d719897699f8b5810189ef32e9220
Anuj-Gupta4 <anujgupta.4388@gmail.com>
2023-02-27 21:17:28 +0545

    seperated css file for styling

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -147,6 +147,7 @@ LOGIN_REDIRECT_URL = 'listing_list'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static/'),
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'media'
