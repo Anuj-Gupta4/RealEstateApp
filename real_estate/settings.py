07191338713865442e5bc13e7b9c6b64113963a4
Anuj-Gupta4 <anujgupta.4388@gmail.com>
2023-03-09 22:04:42 +0545

    .

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -166,5 +166,5 @@ EMAIL_HOST = 'smtp.gmail.com' # or any other email service provider host
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = env('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = 'anujgupta.4388@gmail.com'
+EMAIL_HOST_PASSWORD = 'ocbqlwmtqzvnpolh'
